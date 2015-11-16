d64d77bccd965c68f3defe77873bdb3629af89a7
Matthew Tole <mtole@pebble.com>
2015-11-16 09:15:32 -0800

    Add some commands to the JIRA bot and re-engineer a little

--- message-handlers.js
+++ message-handlers.js
@@ -7,6 +7,7 @@ module.exports.iAmJiraUser = function(ctx, message, callback) {
   if (!ctx.isMessageForMe(message)) {
     return callback(null, false);
   }
+  const channel = ctx.slack.getChannelGroupOrDMByID(message.channel);
 
   const data = /i am ([0-9a-z\.]+)/i.exec(message.text);
   if (!data) {
@@ -14,8 +15,14 @@ module.exports.iAmJiraUser = function(ctx, message, callback) {
   }
 
   const username = data[1];
-  ctx.jiraUsers[message.user] = username;
-  return callback(null, true);
+  ctx.setJiraUserForSlackUser(message.user, username, err => {
+    if (err) {
+      channel.send(`Failed to set the JIRA username of ${ctx.slack.getUserByID(message.user).name} to ${username}`);
+    } else {
+      channel.send(`Successfully set the JIRA username of ${ctx.slack.getUserByID(message.user).name} to ${username}`);
+    }
+    return callback(err, true);
+  });
 };
 
 module.exports.assignIssue = function(ctx, message, callback) {
@@ -26,11 +33,6 @@ module.exports.assignIssue = function(ctx, message, callback) {
 
   const data = /assign (that|([A-Z]{2,8}-[0-9]{1,8})) to (me|(<@[0-9A-Z]*>))$/i.exec(message.text); // eslint-disable-line
   const assignData = {};
-  if (data[1] === 'that') {
-    assignData.issue = ctx.getLastIssue(message.channel);
-  } else {
-    assignData.issue = data[1];
-  }
 
   if (data[3] === 'me') {
     assignData.assignee = message.user;
@@ -38,27 +40,43 @@ module.exports.assignIssue = function(ctx, message, callback) {
     assignData.assignee = data[3].substr(2, data[3].length - 3);
   }
 
-  if (!ctx.jiraUsers[assignData.assignee]) {
-    channel.send(`Sorry, I do not know the JIRA username for ${ctx.slack.getUserByID(assignData.assignee).name}.`);
-  } else {
-    ctx.jira.updateIssue(assignData.issue, {
-      assignee: ctx.jiraUsers[assignData.assignee]
-    }, err => {
+  if (data[1] === 'that') {
+    ctx.getLastIssue(message.channel, (err, issue) => {
       if (err) {
-        console.log(err);
+        return callback(err);
       }
-      channel.send(`Okay! I have assigned ${assignData.issue} to ${ctx.slack.getUserByID(assignData.assignee).name}`);
+      assignData.issue = issue;
+      doAssignment(callback);
+    });
+  } else {
+    assignData.issue = data[1];
+    doAssignment(callback);
+  }
+
+  function doAssignment(done) {
+    ctx.getJiraUserFromSlackUser(assignData.assignee, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        channel.send(`Sorry, I do not know the JIRA username for ${ctx.slack.getUserByID(assignData.assignee).name}.`);
+        return done(null, false);
+      } else {
+        ctx.jira.updateIssue(assignData.issue, {
+          fields: {
+            assignee: { name: user }
+          }
+        }, err => {
+          if (err) {
+            channel.send(`Oh no! I could not assign ${assignData.issue} to ${ctx.slack.getUserByID(assignData.assignee).name}`);
+            return done(err);
+          }
+          channel.send(`Okay! I have assigned ${assignData.issue} to ${ctx.slack.getUserByID(assignData.assignee).name}`);
+          return done(null, false);
+        });
+      }
     });
   }
-};
-
-module.exports.markIssue = function(ctx, message, callback) {
-  const channel = ctx.slack.getChannelGroupOrDMByID(message.channel);
-
-  if (!ctx.isMessageForMe(message)) {
-    return callback(null, false);
-  }
-
 };
 
 module.exports.jiraInfo = function(ctx, message, callback) {
